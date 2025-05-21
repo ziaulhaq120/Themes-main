39c935511ae48175f6c693069378d00f273a2bbd
ziaulhaq <ziaaajr@gmail.com>
2025-05-21 13:21:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "cifumo" ]; then
+  if [ "$USER_TOKEN" = "zia" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}chat dulu Gih Ke cifumo{NC}"
